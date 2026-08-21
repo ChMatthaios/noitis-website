d796c59b2afed5380206595f89633003c4f6d9a7
Matthaios Ch. <34523286+ChMatthaios@users.noreply.github.com>
2026-08-21 14:51:27 +0300

    Build all public legal pages

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
       input: {
         main: fileURLToPath(new URL('./index.html', import.meta.url)),
         privacy: fileURLToPath(new URL('./privacy.html', import.meta.url)),
+        terms: fileURLToPath(new URL('./terms.html', import.meta.url)),
+        trademark: fileURLToPath(new URL('./trademark.html', import.meta.url)),
       },
     },
   },
